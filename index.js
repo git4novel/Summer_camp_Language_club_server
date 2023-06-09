657c70360435037a6cee01a37ff07054180aa13f
git4novel <novelsylhet@gmail.com>
2023-06-09 18:20:35 +0600

    after adding basic mongo connection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
@@ -12,3 +13,45 @@ app.use(cors())
 app.use(express.json())
 
 // will write verify jwt out of mongodb
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqha5r5.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //this line might create problem
+    await client.connect();
+
+    // collection here
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//---------------------------------------------
+app.get('/', (req, res)=>{
+    res.send('Summer camp server is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`App is Running on ${port}`);
+})
