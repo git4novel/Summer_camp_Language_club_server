d450f66ed572fcada4f37bc632c900b1943e170e
git4novel <novelsylhet@gmail.com>
2023-06-16 16:31:13 +0600

    payment

--- index.js
+++ index.js
@@ -182,6 +182,23 @@ async function run() {
 
 
 
+    // --payment related
+     //-create  payment intent
+     app.post('/create-payment-intent', async(req, res)=>{
+      const {price} = req.body;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+
     // instructors get operation
     app.get('/instructors', async(req, res)=>{
       const result = await instructorCollection.find().toArray()
